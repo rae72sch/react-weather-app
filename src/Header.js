04a90d786d9b450ffbe9c8c82de7b03e8f4b7efe
Rachel Schofield <rae72sch@hotmail.com>
2024-03-04 16:46:33 +0000

    Added basic Header component

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Dublin</h1>
+      <div className="big-weather-emoji"> 🌤️ </div>
+    </div>
+  );
+}
