962fba0955fc53f518365523aa10694385459d60
Rachel Schofield <rae72sch@hotmail.com>
2024-03-04 16:47:39 +0000

    Added basic Main component

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="main">
+      <div className="left-column">
+        Partly cloudy
+        <br />
+        Feels like 12&deg;
+      </div>
+      <div className="middle-column">
+        <span className="main-temp">6&deg;c</span>
+        <div className="day-time">Tuesday 10:20</div>
+      </div>
+      <div class="right-column">
+        Humidity: 84%
+        <br />
+        Wind speed: 8mph
+      </div>
+    </div>
+  );
+}
